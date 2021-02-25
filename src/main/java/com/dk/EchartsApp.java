2b1ec547060dbd401a9244eb72c92c1ccb0368c1
renu311 <wsc34@qq.com>
2021-02-25 18:31:56 +0800

    代码提交

---
+++ src/main/java/com/dk/EchartsApp.java
@@ -0,0 +1,18 @@
+package com.dk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Deacription TODO
+ * @Author Suncheer
+ * @Date 2021/1/7 9:49
+ * @Version 1.0
+ **/
+@SpringBootApplication
+public class EchartsApp {
+  public static void main(String[] args) {
+    SpringApplication.run(EchartsApp.class,args);
+  }
+}
+
